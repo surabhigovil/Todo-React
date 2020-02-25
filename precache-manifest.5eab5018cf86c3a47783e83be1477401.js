f0589e26ba028d62c22f5da256edabf517fddaf6
Surabhi Govil <govil.surabhi@gmail.com>
2020-02-25 11:13:55 -0800

    Updates

---
+++ precache-manifest.5eab5018cf86c3a47783e83be1477401.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c197ae69f0376250f9d088c19fadf473",
+    "url": "/Todo-React/index.html"
+  },
+  {
+    "revision": "014d3cad7a3e3f948ee0",
+    "url": "/Todo-React/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d3031424cb1673bc9821",
+    "url": "/Todo-React/static/js/2.1245bcca.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Todo-React/static/js/2.1245bcca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014d3cad7a3e3f948ee0",
+    "url": "/Todo-React/static/js/main.34f5c14b.chunk.js"
+  },
+  {
+    "revision": "2e4419bf0391e2164c12",
+    "url": "/Todo-React/static/js/runtime-main.b4ee5046.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Todo-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
